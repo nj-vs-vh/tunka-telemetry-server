b3e5f72331c74571d60ee63a5bae5c95494af772
Igor Vaiman <gosha.vaiman@gmail.com>
2020-11-19 02:20:15 +0300

    Logging fits image saving

--- backend/camera_adapter.py
+++ backend/camera_adapter.py
@@ -131,7 +131,9 @@ class CameraAdapter:
     @handles_errors(lambda e: logging.error(f"Error in FITS saving callback: {e}"))
     @accepts_hdu_list
     def _fits_saving_callback(self, hdul: HDUList):
-        hdul.writeto(FITS_DIR / self._generate_image_name('image', 'fits'))
+        file_path = str(FITS_DIR / self._generate_image_name('image', 'fits'))
+        # logging.info(f'saving FITS image to {file_path}')
+        hdul.writeto(file_path)
 
     async def preview_feed_generator(self):
         """Async generator yielding new preview shots as they arise, for outside use"""
