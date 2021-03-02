a36ab90235399255e0b8673254bb6b0ec63558c9
Igor Vaiman <gosha.vaiman@gmail.com>
2021-03-02 15:59:20 +0300

    bytes overflow proper fix

--- backend/fitsutils.py
+++ backend/fitsutils.py
@@ -10,8 +10,8 @@ from typing import Dict, Any
 def normalize_frame(frame: NDArray, bits: int = 8) -> NDArray:
     frame = frame.astype(float)
     frame_min = frame.min()
-    frame_range = 1.01 * (frame.max() - frame_min)
-    return ((2 ** bits) * (frame - frame_min) / frame_range).astype('int8')
+    frame_range = frame.max() - frame_min
+    return ((2 ** bits - 1) * (frame - frame_min) / frame_range).astype('int8')
 
 
 def save_fits_as_jpeg(hdul: HDUList, filename: str):
