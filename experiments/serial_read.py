7a3f532eb98942ca0b45c7305f98b55e24da177b
Igor Vaiman <gosha.vaiman@gmail.com>
2021-03-02 18:24:57 +0300

    serial read v2

--- experiments/serial_read.py
+++ experiments/serial_read.py
@@ -2,9 +2,6 @@ import serial
 
 ttyname = '/dev/ttyACM0'
 
-# baudrate = 9600  # measured with sudo stty -F /dev/ttyACM0
 with serial.Serial(ttyname) as ser:
-    # line = ser.readline()
-    # print(line)
-    byte = ser.read()
-    print(byte)
+    line = ser.readline()
+    print(line)
