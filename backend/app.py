cac8e4df9ec233ae2e00a591e4aa3b17023816ac
Igor Vaiman <gosha.vaiman@gmail.com>
2021-11-17 13:23:21 +0300

    logs are checked for existence

--- backend/app.py
+++ backend/app.py
@@ -22,17 +22,21 @@ ROOT_DIR = CUR_DIR.parent
 SERVER_LOG = ROOT_DIR / "server.log"
 CAMERA_LOG = ROOT_DIR / "camera.log"
 
-PREVIOUS_LOGS_DIR = ROOT_DIR / 'previous_logs'
+PREVIOUS_LOGS_DIR = ROOT_DIR / "previous_logs"
 PREVIOUS_LOGS_DIR.mkdir(exist_ok=True)
 now_str = datetime.utcnow().isoformat()
 for log in [SERVER_LOG, CAMERA_LOG]:
-    log.rename(PREVIOUS_LOGS_DIR / f"{log.stem}.before.{now_str}.log")
+    if log.exists():
+        log.rename(PREVIOUS_LOGS_DIR / f"{log.stem}.before.{now_str}.log")
 
 # logging setup
 # see https://docs.python.org/3/library/logging.html#logging.basicConfig
-logging_config = {"format": r"[%(asctime)s] %(levelname)s: %(message)s", "datefmt": r"%x %X", "filename": CAMERA_LOG}
-log_level = os.environ.get("LOG_LEVEL", "INFO")
-logging_config["level"] = getattr(logging, log_level)  # log_level.DEBUG, log_level.INFO, etc
+logging_config = {
+    "format": r"[%(asctime)s] %(levelname)s: %(message)s",
+    "datefmt": r"%x %X",
+    "filename": CAMERA_LOG,
+    "level": getattr(logging, os.environ.get("LOG_LEVEL", "INFO")),
+}
 logging.basicConfig(**logging_config)
 
 indigo_debug_settings = os.environ.get("INDIGO_DEBUG", None)
@@ -94,7 +98,7 @@ try:
     PORT = int(os.environ.get("PORT", 8000))
     if serve_with == "Hypercorn":
         config = Config()
-        config.bind = f'0.0.0.0:{PORT}'
+        config.bind = f"0.0.0.0:{PORT}"
         config.workers = 1
         config.errorlog = str(SERVER_LOG.resolve())
         config.accesslog = str(SERVER_LOG.resolve())
